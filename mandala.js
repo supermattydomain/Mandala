e9137787159f6c3ea31b8060a9ae0e5ec9c3b839
Matt <matty@supermatty.com>
2017-01-15 11:12:37 +1100

    Use strict.

--- mandala.js
+++ mandala.js
@@ -5,8 +5,10 @@
  * https://www.gnu.org/licenses/gpl-3.0.en.html
  */
 
+"use strict";
+
 if (typeof Mandala === "undefined") {
-	Mandala = {};
+	var Mandala = {};
 }
 
 Mandala.Mandala = function(selector) {
